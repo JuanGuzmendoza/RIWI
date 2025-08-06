e7f2f08ad7f1e966053288a72934964ba5f25ee3
Juan Guzma <juanguzman10102005@gmail.com>
2025-08-06 11:56:28 -0500

    sql ejercicios

--- Sql.sql
+++ Sql.sql
@@ -56,4 +56,56 @@ SELECT producto, MAX(precio_unitario) AS precio_unitario
 FROM compras;
 
 /* 16 */
+SELECT producto, SUM(cantidad) AS total_unidades, COUNT(*) AS veces_comprado
+FROM compras
+GROUP BY producto;
+
+/* 17 */
+SELECT 
+  marca,
+  AVG(calificacion_compra) AS promedio_calificacion
+FROM compras
+GROUP BY marca
+HAVING AVG(calificacion_compra) > 4;
+
+/* 18 */
+select categoria , sum(cantidad*precio_unitario) as total_gastado
+from compras
+group by categoria;
+
+/* 19 */
+SELECT 
+  ciudad, 
+  COUNT(*) AS total_activos
+FROM clientes
+WHERE activo = '1'
+GROUP BY ciudad
+ORDER BY total_activos DESC
+LIMIT 3;
+
+/* 20 */
+SELECT 
+  clientes.nombre, 
+  COUNT(*) AS total_compras
+FROM compras
+JOIN clientes ON compras.cliente_id = clientes.id
+WHERE clientes.edad > 30
+GROUP BY clientes.id, clientes.nombre;
+
+
+/* 21 */
+SELECT 
+  producto,
+  COUNT(DISTINCT cliente_id) AS cantidad_personas
+FROM compras
+GROUP BY producto
+HAVING COUNT(DISTINCT cliente_id) > 3;
+
+/* 22 */
+SELECT nombre, referido_por
+FROM clientes
+WHERE referido_por IS NOT NULL;
+
+
+
 
